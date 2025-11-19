24ccee0f6d6da239e8f9c07d6762d5e111e49122
Katie Siegel <165830396+Katie-Siegel@users.noreply.github.com>
2025-11-19 17:50:51 -0500

    Added js for generating compliment and pulling random elements

--- js/app.js
+++ js/app.js
@@ -2,10 +2,28 @@ const message = document.querySelector('.compliment-view');
 const compButton = document.querySelector('.compliment-button');
 
 
-const generatedCompliment = function () {
-	
-}
 
+function adjRandom() {
+	const adj = ["smart", "hilarious", "wise", "peculiar", 'loyal'];
+	return adj[Math.floor(Math.random() * adj.length)];
+};
+	
+function nounRandom() {
+	const noun = ["cat", "basket", "fairy", "stallion"];
+	return noun[Math.floor(Math.random() * noun.length)];
+};
+	
+function ideaOneRandomRandom() {
+	const ideaOne = ['Hope', 'Wishes', 'Starlight', 'Questions'];
+	return ideaOne[Math.floor(Math.random() * ideaOne.length)];
+};
+	
+function ideaTwoRandomRandom() {
+	const ideaTwo = [''];
+	return ideaTwo[Math.floor(Math.random() * ideaTwo.length)];
+};
+	
+const generatedCompliment = `You {adjRandom} {nounRandom} of {ideaOneRandom} and {ideaTwoRandom}!`
 
 compButton.addEventListener('click', function (e) {
 	message.innerText = generatedCompliment;
