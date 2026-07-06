b7898df6c1555a1b15ed4a8c1fe2ec28a7df1369
Katie Siegel <165830396+Katie-Siegel@users.noreply.github.com>
2026-07-06 14:45:04 -0400

    Added more content to quoteRandom

--- js/app.js
+++ js/app.js
@@ -53,7 +53,8 @@ function quoteRandom() {
 		'“Friendship is the golden thread that ties the heart of all the world.”', '“A friend is someone who knows all about you and still loves you.”', '“Good friends are like stars. You don’t always see them, but you know they’re always there.”',
 		'“Friendship is born at that moment when one person says to another, ‘What! You too? I thought I was the only one.’”', '“A friend is one of the nicest things you can have, and one of the best things you can be.”',
 		'“Friendship is the only cement that will ever hold the world together.”', '“A real friend is one who walks in when the rest of the world walks out.”', '“A true friend is like a star; you don’t always see them, but you know they’re always there.”',
-		'“Boundaries are our shields; let’s respect and honor them.”'
+		'“Boundaries are our shields; let’s respect and honor them.”', '"A loyal friend laughs at your jokes when they\'re not so good, and sympathizes with your problems when they\'re not so bad." – Arnold H. Glasgow',
+		'"I don’t know what I would have done so many times in my life if I hadn’t had my girlfriends." – Reese Witherspoon', ''
 	];
 	return quote[Math.floor(Math.random() * quote.length)]
 }
